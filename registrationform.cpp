b63da6cb0816ebdd23257b4fff170c787e8b986f
om150605 <shuklaom342@gmail.com>
2025-08-11 22:31:47 +0530

    Add files via upload
    <!DOCTYPE html>
    <html>
    <head>
        <title>Registration Form</title>
    </head>
    <body>
    
        <h2>Registration Form</h2>
    
        <form action="/submit_registration" method="post">
            <label for="fullname">Full Name:</label><br>
            <input type="text" id="fullname" name="fullname" required><br><br>
    
            <label for="email">Email:</label><br>
            <input type="email" id="email" name="email" required><br><br>
    
            <label for="password">Password:</label><br>
            <input type="password" id="password" name="password" required><br><br>
    
            <label for="confirm_password">Confirm Password:</label><br>
            <input type="password" id="confirm_password" name="confirm_password" required><br><br>
    
            <input type="submit" value="Register">
        </form>
    
    </body>
    </html>

---
+++ registrationform.cpp
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Form</title>
+</head>
+<body>
+
+    <h2>Registration Form</h2>
+
+    <form action="/submit_registration" method="post">
+        <label for="fullname">Full Name:</label><br>
+        <input type="text" id="fullname" name="fullname" required><br><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="confirm_password">Confirm Password:</label><br>
+        <input type="password" id="confirm_password" name="confirm_password" required><br><br>
+
+        <input type="submit" value="Register">
+    </form>
+
+</body>
+</html>
